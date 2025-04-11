bff4aabe153037e4003190a386f13d9f4d4703a6
Walter66888 <80444578+Walter66888@users.noreply.github.com>
2025-04-12 01:17:21 +0800

    Update __init__.py

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -4,3 +4,4 @@
 from .fubon_crawler import check_fubon_futures_report, extract_fubon_report_data
 from .sinopac_crawler import check_sinopac_futures_report, extract_sinopac_report_data
 from .utils import get_today_date, is_trading_day, convert_pdf_to_text, convert_pdf_to_images
+from .futures_data_extractor import extract_pdf_data
