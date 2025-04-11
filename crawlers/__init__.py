85aac32ca329031f81103e946364cf50a97343fd
Walter66888 <80444578+Walter66888@users.noreply.github.com>
2025-04-11 22:40:39 +0800

    Create __init__.py

---
+++ crawlers/__init__.py
@@ -0,0 +1,6 @@
+"""
+爬蟲模組初始化
+"""
+from .fubon_crawler import check_fubon_futures_report, extract_fubon_report_data
+from .sinopac_crawler import check_sinopac_futures_report, extract_sinopac_report_data
+from .utils import get_today_date, is_trading_day, convert_pdf_to_text, convert_pdf_to_images
