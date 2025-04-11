d9007fda4b3e7c4983bd52c315b421f6db3226e4
Walter66888 <80444578+Walter66888@users.noreply.github.com>
2025-04-11 23:32:22 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,5 @@
 """
 處理程序模組初始化
 """
-from .line_handler import handle_line_message, send_latest_report, send_pdf_to_line
+from .line_handler import handle_line_message, send_latest_report
 from .report_handler import monitor_futures_reports, generate_report_text, get_latest_report_data
